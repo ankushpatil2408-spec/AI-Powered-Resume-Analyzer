305d9735de0a7b8140b1216ac626279849d02b92
Ankush Shriram Patil <ankushpatil.2408@gmail.com>
2026-07-21 22:43:56 +0530

    Fix syntax error in appService.java

--- Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
+++ Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
@@ -30,7 +30,7 @@ public class appService {
     @Value("${GEMINI_API_KEY}")
     private String genKey;
 
-    @Value("${application-id}"
+    @Value("${application-id}")
     private String applicationId;
 
     @Value("${application-api-key}")
