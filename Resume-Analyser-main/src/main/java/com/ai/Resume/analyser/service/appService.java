3cbc0199894edcb14a6b79fbcfafe60876dc2da4
Ankush Shriram Patil <ankushpatil.2408@gmail.com>
2026-07-21 22:26:19 +0530

    Update application API key property name

--- Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
+++ Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
@@ -33,7 +33,7 @@ public class appService {
     @Value("${ADZUNA_APP_ID}")
     private String applicationId;
 
-    @Value("${ADZUNA_API_KEY}")
+    @Value("${application-api-key}")
     private String applicationApiKey;
 
     @Autowired
