ea1696ed446495e2132097e0efe49d6f0847386d
Ankush Shriram Patil <ankushpatil.2408@gmail.com>
2026-07-21 22:28:16 +0530

    Fix application ID property key in appService.java

--- Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
+++ Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
@@ -30,7 +30,7 @@ public class appService {
     @Value("${GEMINI_API_KEY}")
     private String genKey;
 
-    @Value("${ADZUNA_APP_ID}")
+    @Value("${application-id=}"
     private String applicationId;
 
     @Value("${application-api-key}")
