11732fed94164c21a06903929d8f0ca1654b1598
Ankush Shriram Patil <ankushpatil.2408@gmail.com>
2026-07-21 22:19:49 +0530

    Rename BREVO_API_KEY to ADZUNA_API_KEY

--- Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
+++ Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/appService.java
@@ -33,7 +33,7 @@ public class appService {
     @Value("${ADZUNA_APP_ID}")
     private String applicationId;
 
-    @Value("${BREVO_API_KEY}")
+    @Value("${ADZUNA_API_KEY}")
     private String applicationApiKey;
 
     @Autowired
