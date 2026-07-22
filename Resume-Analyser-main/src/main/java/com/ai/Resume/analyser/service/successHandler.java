a9262bcb51b7c0b9eda39f8875b81dc27ead2c02
Ankush Shriram Patil <ankushpatil.2408@gmail.com>
2026-07-22 13:41:02 +0530

    Update cookie settings for entrypasstoken

--- Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/successHandler.java
+++ Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/service/successHandler.java
@@ -48,7 +48,7 @@ public class successHandler implements AuthenticationSuccessHandler {
         }
 
         String token = jwtService.generateToken(email);
-        ResponseCookie cookie = ResponseCookie.from("entrypasstoken",token).path("/").httpOnly(true).maxAge(20*24*60*60).sameSite("Strict").secure(false).build();
+        ResponseCookie cookie = ResponseCookie.from("entrypasstoken",token).path("/").httpOnly(true).maxAge(20*24*60*60).sameSite("None").secure(true).build();
         response.addHeader("Set-Cookie",cookie.toString());
         response.sendRedirect("/");
 
