a8242fafd1cb0c1f8b53abf965f4408b7d614d42
Ankush Shriram Patil <ankushpatil.2408@gmail.com>
2026-07-22 20:14:59 +0530

    Add Optional import for usersTableRepo

--- Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/repository/usersTableRepo.java
+++ Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/repository/usersTableRepo.java
@@ -5,6 +5,8 @@ import com.ai.Resume.analyser.model.usersTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface usersTableRepo extends JpaRepository<usersTable,String> {
    Optional<usersTable> findByEmail(String email);
