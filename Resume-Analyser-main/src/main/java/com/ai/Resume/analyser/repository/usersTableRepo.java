547f72dac398643ee383eabc88a08d1ca950d131
Ankush Shriram Patil <ankushpatil.2408@gmail.com>
2026-07-22 20:07:37 +0530

    Add method to find user by email in repository

--- Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/repository/usersTableRepo.java
+++ Resume-Analyser-main/src/main/java/com/ai/Resume/analyser/repository/usersTableRepo.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface usersTableRepo extends JpaRepository<usersTable,String> {
+   Optional<usersTable> findByEmail(String email);
 }
